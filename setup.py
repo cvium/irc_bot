486c8c3a0cca3563653a578f6793e32551cfe281
cvium <clausvium@gmail.com>
2016-07-18 14:56:47 +0200

    added pypi stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='irc_bot',
+  packages=['irc_bot'],
+  version='1.0',
+  description='A small library to create an IRC bot. Uses asyncore to ensure compatibility with Python 2.7.',
+  author='Claus Vium',
+  author_email='clausvium@gmail.com',
+  url='https://github.com/cvium/irc_bot',
+  download_url='https://github.com/cvium/irc_bot/tarball/1.0',  # I'll explain this in a second
+  keywords=['irc', 'bot'],  # arbitrary keywords
+  classifiers=[],
+)
