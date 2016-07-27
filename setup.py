fac3be8b4d9e00b0a85bbcac2853edf83b7f3fef
cvium <clausvium@gmail.com>
2016-07-27 21:32:26 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ def read(fname):
 setup(
     name='irc_bot',
     packages=['irc_bot'],
-    version='1.0.2',
+    version='1.0.3',
     description='A small library to create an IRC bot. Uses asyncore to ensure compatibility with Python 2.7+.',
     author='Claus Vium',
     author_email='clausvium@gmail.com',
     url='https://github.com/cvium/irc_bot',
-    download_url='https://github.com/cvium/irc_bot/tarball/1.0.2',  # I'll explain this in a second
+    download_url='https://github.com/cvium/irc_bot/tarball/1.0.3',  # I'll explain this in a second
     keywords=['irc', 'bot'],  # arbitrary keywords
     classifiers=[
         'Intended Audience :: Developers',
