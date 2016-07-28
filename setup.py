0e0f08e7f748480a11762b1fc1a2f93775a8ad6b
cvium <clausvium@gmail.com>
2016-07-28 10:12:48 +0200

    fix deployment

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup
 import os
+import sys
+import io
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+# Populates __version__ without importing the package
+__version__ = None
+with io.open('irc_bot/__version__.py', encoding='utf-8')as ver_file:
+    exec(ver_file.read())  # pylint: disable=W0122
+if not __version__:
+    print('Could not find __version__ from irc_bot/__version__.py')
+    sys.exit(1)
+
+
 setup(
     name='irc_bot',
     packages=['irc_bot'],
-    version='1.0.3',
+    version=__version__,
     description='A small library to create an IRC bot. Uses asyncore to ensure compatibility with Python 2.7+.',
     author='Claus Vium',
     author_email='clausvium@gmail.com',
     url='https://github.com/cvium/irc_bot',
-    download_url='https://github.com/cvium/irc_bot/tarball/1.0.3',  # I'll explain this in a second
+    #download_url='https://github.com/cvium/irc_bot/tarball/__version__',
     keywords=['irc', 'bot'],  # arbitrary keywords
     classifiers=[
         'Intended Audience :: Developers',
